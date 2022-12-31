96d75491e18378b020f9648ddb53d825ac82662c
Liron Kruchinin <lironkruch@gmail.com>
2022-12-31 18:22:39 +0200

    home page

--- views/about.jsx
+++ views/about.jsx
@@ -1,6 +1,23 @@
+import { } from "../assets/"
 
 export function About() {
     return <section className="about">
-        <h1>About Page</h1>
+        <div className="img-content">
+            <img src="assets\img\Portfolio-website.svg" alt="" />
+        </div>
+
+        <div className="programmer-data">
+            <div className="prog">
+                <img src="assets\img\Detective.svg" alt="" />
+                <h3>Neal Adam</h3>
+                <h4>Fullstack developer</h4>
+            </div>
+
+            <div className="prog">
+                <img src="assets\img\Design.svg" alt="" />
+                <h3>Liron Kruchinin</h3>
+                <h4>Fullstack developer</h4>
+            </div>
+        </div>
     </section>
 }
--- views/home.jsx
+++ views/home.jsx
@@ -1,15 +1,22 @@
+const { Link } = ReactRouterDOM
+
+
 export function Home() {
 
     return <section className="home">
         <h1>Welcome to App sus, where you can use our mailing and notes system</h1>
         <div className="svg-container">
             <div className="svg-content">
-                <img src="assets\img\Mail.svg" alt="" />
+                <Link to="/mail">
+                    <img src="assets\img\Mail.svg" alt="" />
+                </Link>
                 <h2>Mails</h2>
 
             </div>
             <div className="svg-content">
-                <img src="assets\img\Tasks.svg" alt="" />
+                <Link to="/note">
+                    <img src="assets\img\Tasks.svg" alt="" />
+                </Link>
                 <h2>Notes</h2>
             </div>
         </div>
